50f58db2cc9b9554712ec5296c3cf029419aa66d
Lucas Azevedo Benedito <lucas.azevedo2104@gmail.com>
2026-09-24 13:40:46 -0300

    style: graphite low-glare angular theme for storefront and account screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 
 import "./App.css";
 import "./modernizacao.css";
+import "./refinamento-v5.css";
 
 import Admin from "./Admin";
 import Login from "./Login";
@@ -819,13 +820,13 @@ function App() {
 
   if (mostrarCadastro) {
     return (
-      <Cadastro
+      <div className="valt-auth-shell"><Cadastro
         onCadastroSucesso={() => {
           setMostrarCadastro(false);
           setMostrarLogin(true);
         }}
         onVoltar={() => setMostrarCadastro(false)}
-      />
+      /></div>
     );
   }
 
